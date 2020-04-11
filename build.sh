9580be487421b743ed3d2fa7a84f982203b250e3
Pawel Cioch <pawel@greenhex.net>
2020-04-11 15:36:19 -0500

    auto build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+./autogen.sh
+make -C depends
+./configure --prefix=`pwd`/depends/x86_64-pc-linux-gnu --with-gui --disable-tests
+make
+cd src
+strip syscoind syscoin-cli syscoin-tx syscoin-wallet
+cd qt
+strip xax-qt
+
